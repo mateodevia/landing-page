a7a2ccd8121e41ea12a3681db8b42073d0e3cbb9
mateodevia <MY_NAME@example.com>
2021-04-11 11:52:13 -0500

    First version of my skills graph

---
+++ components/GraphGenerator.js
@@ -0,0 +1,206 @@
+import * as d3 from "d3";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import styles from "./forceGraph.module.css";
+import { filter } from "d3";
+
+export function runForceGraph(
+  container,
+  linksData,
+  nodesData,
+  nodeHoverTooltip
+) {
+  const links = linksData.map((d) => Object.assign({}, d));
+  const nodes = nodesData.map((d) => Object.assign({}, d));
+
+  const containerRect = container.getBoundingClientRect();
+  const height = containerRect.height;
+  const width = containerRect.width;
+
+  const color = (d) => {
+    return d.color;
+  };
+
+  const getClass = (d) => {
+    return styles["text" + d.text];
+  };
+
+  const drag = (simulation) => {
+    const dragstarted = (e, d) => {
+      if (!e.active) simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    };
+
+    const dragged = (e, d) => {
+      d.fx = e.x;
+      d.fy = e.y;
+    };
+
+    const dragended = (e, d) => {
+      if (!e.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    };
+
+    return d3
+      .drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+  };
+
+  // Add the tooltip element to the graph
+  const tooltip = document.querySelector("#graph-tooltip");
+  if (!tooltip) {
+    const tooltipDiv = document.createElement("div");
+    tooltipDiv.classList.add(styles.tooltip);
+    tooltipDiv.style.opacity = "0";
+    tooltipDiv.id = "graph-tooltip";
+    document.body.appendChild(tooltipDiv);
+  }
+  const div = d3.select("#graph-tooltip");
+
+  const addTooltip = (hoverTooltip, d, x, y) => {
+    div.transition().duration(200).style("opacity", 0.9);
+    div
+      .html(hoverTooltip(d))
+      .style("background-color", `grey`)
+      .style("color", `white`)
+      .style("left", `${x}px`)
+      .style("top", `${y - 28}px`);
+  };
+
+  const removeTooltip = () => {
+    div.transition().duration(200).style("opacity", 0);
+  };
+
+  const simulation = d3
+    .forceSimulation(nodes)
+    .force(
+      "link",
+      d3
+        .forceLink(links)
+        .id((d) => d.id)
+        .strength(3)
+        .distance((d) => d.value + d.source.size / 2)
+    )
+    .force("charge", d3.forceManyBody())
+    .force(
+      "collision",
+      d3.forceCollide().radius((d) => d.size / 2 + 5)
+    )
+    .force("x", d3.forceX())
+    .force("y", d3.forceY());
+
+  const svg = d3
+    .select(container)
+    .append("svg")
+    .attr("viewBox", [-width / 2, -height / 2, width, height])
+    .call(
+      d3.zoom().on("zoom", function (e) {
+        svg.attr("transform", e.transform);
+      })
+    );
+
+  const link = svg
+    .append("g")
+    .attr("stroke", "#999")
+    .attr("stroke-opacity", 0.6)
+    .selectAll("line")
+    .data(links)
+    .join("line")
+    .attr("stroke-width", 2);
+
+  const node = svg
+    .append("g")
+    .attr("stroke", "#fff")
+    .attr("stroke-width", 2)
+    .selectAll("circle")
+    .data(nodes)
+    .join("circle")
+    .attr("r", (d) => d.size / 2)
+    .attr("fill", color)
+    .call(drag(simulation));
+
+  const label = svg
+    .append("g")
+    .attr("class", "labels")
+    .selectAll("text")
+    .data(nodes.filter((n) => n.type === "subject"))
+    .enter()
+    .append("foreignObject");
+  label
+    .attr("width", (d) => d.size)
+    .attr("height", (d) => d.size)
+    .append("xhtml:div")
+    .attr("class", `flexbox ${styles.subjectContainer}`)
+    .html((d) => {
+      return `<div class="${styles.subjectLabel} ${getClass(d)}">${d.id}</div>`;
+    })
+    .call(drag(simulation));
+
+  const icon = svg
+    .append("g")
+    .attr("class", "labels")
+    .selectAll("text")
+    .data(nodes.filter((n) => n.type === "tool"))
+    .enter()
+    .append("svg:image")
+    .attr("xlink:href", (d) => {
+      return d.icon;
+    })
+    .attr("height", (d) => {
+      return d.size * 0.6;
+    })
+    .attr("width", (d) => {
+      return d.size * 0.6;
+    })
+    .call(drag(simulation));
+
+  icon
+    .on("mouseover", (e, d) => {
+      addTooltip(nodeHoverTooltip, d, e.pageX, e.pageY);
+    })
+    .on("mouseout", () => {
+      removeTooltip();
+    });
+
+  simulation.on("tick", () => {
+    //update link positions
+    link
+      .attr("x1", (d) => d.source.x)
+      .attr("y1", (d) => d.source.y)
+      .attr("x2", (d) => d.target.x)
+      .attr("y2", (d) => d.target.y);
+
+    // update node positions
+    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+
+    // update label positions
+    label
+      .attr("x", (d) => {
+        return d.x - d.size / 2;
+      })
+      .attr("y", (d) => {
+        return d.y - d.size / 2;
+      });
+
+    // update icon positions
+    icon
+      .attr("x", (d) => {
+        return d.x - (d.size * 0.6) / 2;
+      })
+      .attr("y", (d) => {
+        return d.y - (d.size * 0.6) / 2;
+      });
+  });
+
+  return {
+    destroy: () => {
+      simulation.stop();
+    },
+    nodes: () => {
+      return svg.node();
+    },
+  };
+}
---
+++ components/SkillsGraph.jsx
@@ -0,0 +1,445 @@
+import React, { useEffect, useRef, useCallback } from "react";
+import { runForceGraph } from "../components/GraphGenerator";
+import styles from "./forceGraph.module.css";
+
+const data = {
+  nodes: [
+    {
+      id: "Web development",
+      size: 160,
+      color: "#5bd4a3",
+      type: "subject",
+      text: "White",
+      group: 1,
+    },
+    {
+      id: "Back-end",
+      size: 110,
+      color: "#313699",
+      type: "subject",
+      text: "White",
+      group: 1,
+    },
+    {
+      id: "Front-end",
+      size: 130,
+      color: "#f0a04a",
+      type: "subject",
+      text: "White",
+      group: 1,
+    },
+    {
+      id: "Vue",
+      size: 80,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/vue.svg",
+      group: 1,
+    },
+    {
+      id: "React",
+      size: 100,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/react.svg",
+      group: 1,
+    },
+    {
+      id: "HTML",
+      size: 80,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/html.svg",
+      group: 1,
+    },
+    {
+      id: "CSS",
+      size: 90,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/css.svg",
+      group: 1,
+    },
+    {
+      id: "JavaScript",
+      size: 110,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/javascript.svg",
+      group: 1,
+    },
+    {
+      id: "Software architecture",
+      size: 110,
+      color: "#fcd303",
+      type: "subject",
+      text: "Black",
+      group: 1,
+    },
+    {
+      id: "Testing",
+      size: 70,
+      color: "#e8314c",
+      type: "subject",
+      text: "White",
+      group: 1,
+    },
+    {
+      id: "UX",
+      size: 50,
+      color: "#9d24ad",
+      type: "subject",
+      text: "White",
+      group: 1,
+    },
+    {
+      id: "UI",
+      size: 50,
+      color: "#e831a2",
+      type: "subject",
+      text: "White",
+      group: 1,
+    },
+    {
+      id: "NextJS",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/nextjs.svg",
+      group: 1,
+    },
+    {
+      id: "Cloud computing",
+      size: 90,
+      color: "#73a9f5",
+      type: "subject",
+      text: "White",
+      group: 1,
+    },
+    {
+      id: "AWS",
+      size: 70,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/aws.svg",
+      group: 1,
+    },
+    {
+      id: "Node JS",
+      size: 90,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/nodejs.svg",
+      group: 1,
+    },
+    {
+      id: "Django",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/django.svg",
+      group: 1,
+    },
+    {
+      id: "Mongo",
+      size: 70,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/mongo.svg",
+      group: 1,
+    },
+    {
+      id: "PostgreSQL",
+      size: 70,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/postgresql.svg",
+      group: 1,
+    },
+    {
+      id: "Python",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/python.svg",
+      group: 1,
+    },
+    {
+      id: "Rabbit MQ",
+      size: 60,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/rabbitmq.svg",
+      group: 1,
+    },
+    {
+      id: "Circle CI",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/circleci.svg",
+      group: 1,
+    },
+    {
+      id: "Cypress",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/cypress.svg",
+      group: 1,
+    },
+    {
+      id: "Firebase",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/firebase.svg",
+      group: 1,
+    },
+    {
+      id: "GitHub",
+      size: 60,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/github.svg",
+      group: 1,
+    },
+    {
+      id: "Heroku",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/heroku.svg",
+      group: 1,
+    },
+    {
+      id: "Java",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/java.svg",
+      group: 1,
+    },
+    {
+      id: "TypeScript",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/typescript.svg",
+      group: 1,
+    },
+    {
+      id: "GraphQL",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/graphql.svg",
+      group: 1,
+    },
+    {
+      id: "Mocha",
+      size: 40,
+      color: "#f5f5f5",
+      type: "tool",
+      icon: "/mocha.svg",
+      group: 1,
+    },
+  ],
+  links: [
+    {
+      source: "Web development",
+      target: "Front-end",
+      value: 200,
+    },
+    {
+      source: "Web development",
+      target: "Back-end",
+      value: 200,
+    },
+    {
+      source: "Testing",
+      target: "Web development",
+      value: 200,
+    },
+    {
+      source: "React",
+      target: "Front-end",
+      value: 5,
+    },
+    {
+      source: "React",
+      target: "NextJS",
+      value: 0,
+    },
+    {
+      source: "Vue",
+      target: "Front-end",
+      value: 5,
+    },
+    {
+      source: "HTML",
+      target: "Front-end",
+      value: 5,
+    },
+    {
+      source: "CSS",
+      target: "Front-end",
+      value: 5,
+    },
+    {
+      source: "JavaScript",
+      target: "Front-end",
+      value: 10,
+    },
+    {
+      source: "JavaScript",
+      target: "Back-end",
+      value: 15,
+    },
+    {
+      source: "UX",
+      target: "Front-end",
+      value: 30,
+    },
+    {
+      source: "UI",
+      target: "Front-end",
+      value: 30,
+    },
+    {
+      source: "Software architecture",
+      target: "Web development",
+      value: 100,
+    },
+    {
+      source: "Software architecture",
+      target: "Cloud computing",
+      value: 100,
+    },
+    {
+      source: "Web development",
+      target: "Cloud computing",
+      value: 100,
+    },
+    {
+      source: "Software architecture",
+      target: "AWS",
+      value: 5,
+    },
+    {
+      source: "Cloud computing",
+      target: "AWS",
+      value: 5,
+    },
+    {
+      source: "JavaScript",
+      target: "Node JS",
+      value: 5,
+    },
+    {
+      source: "Back-end",
+      target: "Node JS",
+      value: 5,
+    },
+    {
+      source: "Back-end",
+      target: "Django",
+      value: 5,
+    },
+    {
+      source: "Back-end",
+      target: "Python",
+      value: 5,
+    },
+    {
+      source: "Django",
+      target: "Python",
+      value: 5,
+    },
+    {
+      source: "Back-end",
+      target: "Mongo",
+      value: 5,
+    },
+    {
+      source: "Back-end",
+      target: "Rabbit MQ",
+      value: 5,
+    },
+    {
+      source: "Back-end",
+      target: "TypeScript",
+      value: 5,
+    },
+    {
+      source: "Back-end",
+      target: "PostgreSQL",
+      value: 5,
+    },
+    {
+      source: "Back-end",
+      target: "GraphQL",
+      value: 5,
+    },
+    {
+      source: "Back-end",
+      target: "Java",
+      value: 5,
+    },
+    {
+      source: "Testing",
+      target: "Cypress",
+      value: 5,
+    },
+    {
+      source: "Testing",
+      target: "Mocha",
+      value: 5,
+    },
+    {
+      source: "Testing",
+      target: "Circle CI",
+      value: 5,
+    },
+    {
+      source: "Cloud computing",
+      target: "Heroku",
+      value: 5,
+    },
+    {
+      source: "Cloud computing",
+      target: "Firebase",
+      value: 5,
+    },
+    {
+      source: "Web development",
+      target: "GitHub",
+      value: 5,
+    },
+  ],
+};
+
+export default function ForceGraph() {
+  const containerRef = useRef(null);
+  useEffect(() => {
+    let destroyFn;
+
+    if (containerRef.current) {
+      const { destroy } = runForceGraph(
+        containerRef.current,
+        data.links,
+        data.nodes,
+        nodeHoverTooltip
+      );
+      destroyFn = destroy;
+    }
+
+    return destroyFn;
+  }, []);
+
+  const nodeHoverTooltip = useCallback((node) => {
+    return `<div>${node.id}</div>`;
+  }, []);
+
+  return <div ref={containerRef} className={styles.container} />;
+}
