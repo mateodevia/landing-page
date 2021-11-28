073754facea38d0b231d60204a7679867dc63fea
mateodevia <MY_NAME@example.com>
2021-11-28 15:16:21 -0500

    configured pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
-// const { i18n } = require('./next-i18next.config')
+const withPWA = require("next-pwa");
 
-module.exports = {
-  // i18n,
-};
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+  },
+});
