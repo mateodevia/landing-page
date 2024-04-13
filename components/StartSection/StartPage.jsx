54ee6dbe702f58ad1a0175d85f1b1f89fa994b4a
mateodevia <MY_NAME@example.com>
2024-04-13 10:15:35 -0500

    updated skills and tyba role

--- components/StartSection/StartPage.jsx
+++ components/StartSection/StartPage.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { useTranslation } from "next-i18next";
-import styles from "./StartPage.module.css";
-import { StaticI18nLink } from "../StaticI18nLink";
+import React from 'react';
+import { useTranslation } from 'next-i18next';
+import styles from './StartPage.module.css';
+import { StaticI18nLink } from '../StaticI18nLink';
 
 const StartPage = (props) => {
-  const { t, i18n } = useTranslation("common");
+  const { t, i18n } = useTranslation('common');
 
   return (
     <div className={`${styles.container} flexbox`}>
-      <StaticI18nLink href='/' locale={i18n.language === "en" ? "es" : "en"}>
+      <StaticI18nLink href='/' locale={i18n.language === 'en' ? 'es' : 'en'}>
         <button className={styles.translation_icon}>
-          <img src={"/icons/translation.png"} />
+          <img src={'/icons/translation.png'} />
         </button>
       </StaticI18nLink>
       <div className={`${styles.start_description_container} flexbox`}>
         <div className={`${styles.text_container}`}>
           <h3>Mateo Devia Vega</h3>
-          <h1>{t("mainTitle")}</h1>
+          <h1>{t('mainTitle')}</h1>
           <div className={`${styles.line} black-line`} />
         </div>
       </div>
@@ -31,15 +31,15 @@ const StartPage = (props) => {
           </div>
           <div className={`${styles.buttons_container} flexbox`}>
             <button className={`${styles.cv} red-button`}>
-              <a href={t("resumeLink")} target='_blank'>
-                {t("myResume")}
+              <a href={t('resumeLink')} target='_blank'>
+                {t('myResume')}
               </a>
             </button>
             <button
               className={`${styles.learn_more} black-button`}
               onClick={props.handleLearnMoreButtonClick}
             >
-              {t("learnMore")}
+              {t('learnMore')}
             </button>
           </div>
         </div>
