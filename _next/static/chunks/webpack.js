690ad65c70c7a8c5b52460f41d0b4cfbab40ff6d
mateodevia <MY_NAME@example.com>
2024-04-13 10:16:11 -0500

    Updates

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "5790dc79bbb699ecc3cb";
+/******/ 	var hotCurrentHash = "6fb95eb89180be6390eb";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
